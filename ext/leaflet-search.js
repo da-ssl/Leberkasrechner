08284e8f76dc38de4a43f9d4580f3f8241c2ac02
Dassl <tassilo@thewalt.de>
2022-04-04 19:41:14 +0200

    Angelsächsiches übersetzt (Englische Elemente in der Leaflet-Search-Extension)

--- ext/leaflet-search.js
+++ ext/leaflet-search.js
@@ -81,9 +81,9 @@ L.Control.Search = L.Control.extend({
 		collapsed: true,				//collapse search control at startup
 		autoCollapse: false,			//collapse search control after submit(on button or on tips if enabled tipAutoSubmit)
 		autoCollapseTime: 1200,			//delay for autoclosing alert and collapse after blur
-		textErr: 'Location not found',	//error message
-		textCancel: 'Cancel',		    //title in cancel button		
-		textPlaceholder: 'Search...',   //placeholder value			
+		textErr: 'Ort nicht gefunden',	//error message
+		textCancel: 'Abbrechen',		//title in cancel button		
+		textPlaceholder: 'Suche...',   	//placeholder value			
 		hideMarkerOnCollapse: false,    //remove circle and marker on search control collapsed		
 		position: 'topleft',		
 		marker: {						//custom L.Marker or false for hide
