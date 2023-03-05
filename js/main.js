721c33db3fa2750b6508fa9a3c42e5b44774e965
CoderKon42 <93076487+CoderKon42@users.noreply.github.com>
2023-03-05 08:48:04 +0100

    Deleted some unnecessary logic and fixed bug
    Deleted some unnecessary logic, which seem to be an relict and fixed bug, that nothing happens for input 7

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 let enoughornot = () => {
-    let execute = true;
     let LeberkasCount = document.getElementById("LeberkasCount").value;
-    let LeberkasCountArr = Array.from(LeberkasCount);
-    
-    if (execute) {
-        if (LeberkasCount < 3) {
-            document.getElementById("answerSection").innerHTML = "Um Gottes Willen! Du brauchst jetzt Leberkäse. Hier findest du Metzgereien:"
-            document.getElementById("meineKarte").style.visibility = "visible";
-        } else if (LeberkasCount < 7) {
-            document.getElementById("answerSection").innerHTML = "<br><br>Basst. Du host gnua Lebakas."
-            document.getElementById("meineKarte").style.visibility = "hidden";
-        } else if (LeberkasCount > 7) {
-            document.getElementById("answerSection").innerHTML = "<br><br>Sappralot! Da werd da ja schlecht!"
-            document.getElementById("meineKarte").style.visibility = "hidden";
-        } else {
-            document.getElementByID("answerSection").innerHTML = "<br><br>Ihre Eingabe konnte leider nicht erkannt werden"
-        }
+
+    if (LeberkasCount < 3) {
+        document.getElementById("answerSection").innerHTML = "Um Gottes Willen! Du brauchst jetzt Leberkäse. Hier findest du Metzgereien:"
+        document.getElementById("meineKarte").style.visibility = "visible";
+    } else if (LeberkasCount < 7) {
+        document.getElementById("answerSection").innerHTML = "<br><br>Basst. Du host gnua Lebakas."
+        document.getElementById("meineKarte").style.visibility = "hidden";
+    } else if (LeberkasCount >= 7) {
+        document.getElementById("answerSection").innerHTML = "<br><br>Sappralot! Da werd da ja schlecht!"
+        document.getElementById("meineKarte").style.visibility = "hidden";
+    } else {
+        document.getElementByID("answerSection").innerHTML = "<br><br>Ihre Eingabe konnte leider nicht erkannt werden"
     }
 }
 
