d35ef798db81c309ad71c5e1b3946fedc62f402a
CoderKon42 <93076487+CoderKon42@users.noreply.github.com>
2023-03-05 09:05:25 +0100

    Update main.js
    made code easier to read and deleted unused function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
 let enoughornot = () => {
     let LeberkasCount = document.getElementById("LeberkasCount").value;
+    let answerSection = document.getElementById("answerSection");
+    let Map = document.getElementById("meineKarte");
 
     if (LeberkasCount < 3) {
-        document.getElementById("answerSection").innerHTML = "Um Gottes Willen! Du brauchst jetzt Leberkäse. Hier findest du Metzgereien:"
-        document.getElementById("meineKarte").style.visibility = "visible";
+        answerSection.innerHTML = "Um Gottes Willen! Du brauchst jetzt Leberkäse. Hier findest du Metzgereien:"
+        Map.style.visibility = "visible";
     } else if (LeberkasCount < 7) {
-        document.getElementById("answerSection").innerHTML = "<br><br>Basst. Du host gnua Lebakas."
-        document.getElementById("meineKarte").style.visibility = "hidden";
+        answerSection.innerHTML = "<br><br>Basst. Du host gnua Lebakas."
+        Map.style.visibility = "hidden";
     } else if (LeberkasCount >= 7) {
-        document.getElementById("answerSection").innerHTML = "<br><br>Sappralot! Da werd da ja schlecht!"
-        document.getElementById("meineKarte").style.visibility = "hidden";
-    } else {
-        document.getElementByID("answerSection").innerHTML = "<br><br>Ihre Eingabe konnte leider nicht erkannt werden"
+        answerSection.innerHTML = "<br><br>Sappralot! Da werd da ja schlecht!"
+        Map.style.visibility = "hidden";
     }
-}
-
-
-let clearanswersection = () => {
-    document.getElementById("answerSection").innerHTML = ""
 }
